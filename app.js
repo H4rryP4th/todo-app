7089421f739c3f4e6c7d259e201a1a7cbff0d322
HarryPath <72655105+H4rryP4th@users.noreply.github.com>
2022-02-26 19:19:43 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const todoController = require('./controllers/todoController')
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('./public'));
+
+// controllers 
+todoController(app);
+
+app.listen(3000);
+console.log('you listen to port 3000');
+
